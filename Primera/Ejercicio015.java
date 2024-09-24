70aed56a015d74959e47d81e816a54228dbcdeab
cris <133981266+cristinasevi@users.noreply.github.com>
2024-09-24 15:46:16 +0200

    Add files via upload

--- Primera/Ejercicio015.java
+++ Primera/Ejercicio015.java
@@ -5,13 +5,14 @@ package Primera;
 
 public class Ejercicio015 {
     public static void main (String arg[]){
-        int datos[] = {17, 33, 400, 95, 170};
-        int mayor = 0;
+        int datos[] = {-17, -33, -400, -95, -170};
+        int mayor = datos[0]; 
+        /* inicializar la variable 'mayor' con el primer elemento del vector, 
+        así se tomará en cuenta cualquier valor que pueda ser mayor */
         
         for(int i=0; i<datos.length; i++)
             if(datos[i] > mayor)
                 mayor = datos[i];
-          
         System.out.println("El número mayor del vector es: " + mayor);  
     }
 }
