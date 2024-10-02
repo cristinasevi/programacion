a5cf7abab89203b7903fe2832c679c27749befad
cris <133981266+cristinasevi@users.noreply.github.com>
2024-10-02 08:53:32 +0200

    Add files via upload

--- Primera/Ejercicio027.java
+++ Primera/Ejercicio027.java
@@ -11,13 +11,13 @@ public class Ejercicio027 {
                           {50, 60, -70, 80},
                           {90, 100, -110, -120} };
         
-        for(int i=0; i<tabla.length; i++)
-            for(int j=0; j<tabla[i].length; j++){
+        for(int i=0; i<tabla.length; i++){
+            for(int j=0; j<tabla[i].length; j++)
                 if(tabla[i][j]<0)
                     tabla[i][j] *= -1;
                 else
                     tabla[i][j] *= 2;  
-                }
+        }
         for(int i=0; i<tabla.length; i++){
             for(int j=0; j<tabla[i].length; j++)
                 System.out.printf("%d ", tabla[i][j]);
