2789ba46770249a6740666dde54a3fbb465512f5
cris <133981266+cristinasevi@users.noreply.github.com>
2024-10-05 11:07:19 +0200

    Add files via upload

--- Primera/Ejercicio035.java
+++ Primera/Ejercicio035.java
@@ -14,23 +14,53 @@ public class Ejercicio035 {
                           {9, 10, 7, 1},
                           {3, 10, 5, 6},
                           {8, 5, 4, 5}  };
-        String alumnos[] = {"Viviana", "Palazón", "Javi", "Vicente", "Oriol"};
+        String alumnos[] = {"Viviana", "Palazón", "Javi", "Vicente", "Álvaro"};
         String asignaturas[] = {"Programación", "Leng.Marcas", "Bases.Datos", "Entornos.Desarrollo"};
         double vAlumnosNota[] = {0, 0, 0, 0, 0};
         double vAsigNota[] = {0, 0, 0, 0};
         
-       for(int i=0; i<vAlumnosNota.length; i++){
-            for(int j=0; j<vAsigNota.length; j++){
-                vAlumnosNota[i] += notas[i][j]/5;
-                vAsigNota[j] += notas[i][j]/4;
+        for(int i=0; i<alumnos.length; i++){
+            for(int j=0; j<asignaturas.length; j++){
+                vAlumnosNota[i] += (double) notas[i][j] / asignaturas.length;
+                vAsigNota[j] += (double) notas[i][j] / alumnos.length;
             }
         }
-        for (int i=0; i<vAlumnosNota.length; i++)
-            System.out.printf("%.2f ", vAlumnosNota[i]);
+       
+        double mejorNotAlum = vAlumnosNota[0];
+        double peorNotAlum = vAlumnosNota[0];
+        String mejorAlum = alumnos[0];
+        String peorAlum = alumnos[0];
+       
+        for (int i = 1; i < alumnos.length; i++) {
+            if (vAlumnosNota[i] > mejorNotAlum) {
+                mejorNotAlum = vAlumnosNota[i];
+                mejorAlum = alumnos[i];
+            }
+            if (vAlumnosNota[i] < peorNotAlum) {
+                peorNotAlum = vAlumnosNota[i];
+                peorAlum = alumnos[i];
+            }
+        }
         
-        System.out.println();
+        double mejorNotAsig = vAsigNota[0];
+        double peorNotAsig = vAsigNota[0];
+        String mejorAsig = asignaturas[0];
+        String peorAsig = asignaturas[0];
         
-        for(int j=0; j<vAsigNota.length; j++)
-            System.out.printf("%.2f ", vAsigNota[j]);  
+        for (int j = 1; j < asignaturas.length; j++) {
+            if (vAsigNota[j] > mejorNotAsig) {
+                mejorNotAsig = vAsigNota[j];
+                mejorAsig = asignaturas[j];
+            }
+            if (vAsigNota[j] < peorNotAsig) {
+                peorNotAsig = vAsigNota[j];
+                peorAsig = asignaturas[j];
+            }
+        }
+        
+        System.out.printf("El alumno con mejor media es %s con una media de %.2f\n", mejorAlum, mejorNotAlum);
+        System.out.printf("El alumno con peor media es %s con una media de %.2f\n", peorAlum, peorNotAlum);
+        System.out.printf("La asignatura con mejor media es %s con una media de %.2f\n", mejorAsig, mejorNotAsig);
+        System.out.printf("La asignatura con peor media es %s con una media de %.2f\n", peorAsig, peorNotAsig);
     }
 }
--- Primera/Ejercicio036.java
+++ Primera/Ejercicio036.java
@@ -6,13 +6,17 @@ package Primera;
 public class Ejercicio036 {
     public static void main(String arg[]) {
         int notas[][] = { {7, 9, 3, 8}, 
-                          {10, 9, 9, 8}, // Palazon soco 10 en programacion
+                          {10, 9, 9, 8}, // Palazon saco 10 en programacion
                           {9, 10, 7, 1}, // javi saco 10 en lenguaje marcas
                           {3, 10, 5, 6}, // vicente saco 10 en lenguaje
                           {8, 5, 4, 5}  };
-        String alumnos[] = {"Viviana", "Palazón", "Javi", "Vicente", "Oriol"};
+        String alumnos[] = {"Viviana", "Palazón", "Javi", "Vicente", "Álvaro"};
         String asignaturas[] = {"Programación", "Leng.Marcas", "Bases.Datos", "Entornos.Desarrollo"};
         
-               
+        
+        for(int i=0; i<alumnos.length; i++)
+            for(int j=0; j<asignaturas.length; j++)
+                if(notas[i][j] == 10) 
+                    System.out.printf("%s sacó un 10 en %s\n", alumnos[i], asignaturas[j]);
     }
 }
