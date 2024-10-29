d7b72405be27e624311487fb6b081e906aa4f9fc
cris <133981266+cristinasevi@users.noreply.github.com>
2024-10-29 16:30:38 +0100

    Add files via upload

--- Primera/Ejercicio062.java
+++ Primera/Ejercicio062.java
@@ -1,6 +1,7 @@
 /*
- Implementar una función que reciba la frase y sustituya el caracter 
- a sustituir por el vector de caracteres sustituto
+ Implementar un método que reciba una cadena de caracteres y sustituya
+ en ella un caracter por otra cadena de caracteres.
+ Lo haremos sobre una cadena de caracteres auxiliar.
  */
 package Primera;
 
@@ -9,23 +10,35 @@ public class Ejercicio062 {
         char frase[] = {'E', 'n', ' ', 'u', 'n', ' ', 'l', 'u', 'g', 'a', 'r', ' ', 'd', 'e', ' ', 'L', 'a', ' ', 'M', 'a', 'n', 'c', 'h', 'a'};
         char sustituir = 'a';
         char sustituto[] = {'X', 'X', 'V'};
-        char frase2[] = new char[100];
         
-        cambiarFrase(frase, sustituir, sustituto, frase2);
+        char frase2[] = cambiarFrase(frase, sustituir, sustituto);
         System.out.println(frase2);
     }  
     
-    public static void cambiarFrase(char frase[], char sustituir, char sustituto[], char frase2[]) {
+    public static char[] cambiarFrase(char frase[], char sustituir, char sustituto[]) {
+        char frase2[] = new char[100];
         int k=0;
         for(int i=0; i<frase.length; i++) {
             if(frase[i] == sustituir) {
                 for(int j=0; j<sustituto.length; j++) {
-                    frase2[k++] = sustituto[j];
+                    frase2[k] = sustituto[j]; 
+                    k++;
                 }
             }
             else {
-                frase2[k++] = frase[i];
+                frase2[k] = frase[i]; 
+                k++;
             }
         }
+        return frase2;
     }
 }
+
+/* 
+ En vez de poner esto:
+ frase2[k] = frase[i];  
+ k++;
+
+ se puede poner directamente esto:
+ frase2[k++] = frase[i];
+*/
