1e4d02fb79e7cd09be1267f618eee30facdb3732
cris <133981266+cristinasevi@users.noreply.github.com>
2024-11-19 12:20:34 +0100

    Update Ejercicio023.java

--- Primera/Ejercicio023.java
+++ Primera/Ejercicio023.java
@@ -8,14 +8,14 @@ public class Ejercicio023 {
     public static void main(String arg[]) {
         int datos[] = {10, 20, 30, 40, 50};
         
-        for(int j=0; j<5; j++) {
+        for(int j=0; j<datos.length; j++) {
             int guarda = datos[datos.length-1];
             for(int i=datos.length-1; i>0; i--)
                 datos[i] = datos[i-1];
             datos[0] = guarda; 
             
            // Mostrar el resultado
-            for(int i=0; i<5; i++)
+            for(int i=0; i<datos.length; i++)
                 System.out.print(datos[i] + " "); 
                 // print --> imprime los elementos en una misma línea 
             System.out.println();
