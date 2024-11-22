521d53037c7c8f2075670e23c0f08fd7b3620c53
cris <133981266+cristinasevi@users.noreply.github.com>
2024-11-22 13:55:07 +0100

    Add files via upload

--- Primera/Ejercicio095.java
+++ Primera/Ejercicio095.java
@@ -5,11 +5,21 @@
 package Primera;
 
 public class Ejercicio095 {
+    /*
+     Atributos de la clase: (están debajo de public class, no están metidos en el public static)
+     Son variables globales que todas las clases pueden acceder a ellas
+    */
+    public static String medidas[] = {"Unidades", "Decenas", "Centenas", "Unidades de millar", "Decenas de millar", "Centenas de millar"};
+    
     public static void main(String arg[]){
-        String medidas[] = {"Unidades", "Decenas", "Centenas", "Unidades de millar", "Decenas de millar", "Centenas de millar"};
+        
         int numero = 256809;
         
         calculo(medidas, numero, 0);
+        
+        System.out.println("----------------------");
+        
+        mostrar(256809, 0);
     }
     
     public static void calculo(String medidas[], int numero, int posicion) {
@@ -19,6 +29,14 @@ public class Ejercicio095 {
             calculo(medidas, numero/10, posicion+1);
             int resto = numero%10;
             System.out.printf("%d %s\n", resto, medidas[posicion]);
+        }
+    }
+    
+    public static void mostrar(int numero, int i) {
+        if(numero > 0) {
+            // System.out.println((numero%10) + " " + medidas[i]); // si pones aquí el print, imprime en orden directo
+            mostrar(numero/10, i+1);
+            System.out.println((numero%10) + " " + medidas[i]); // si pones aquí el print, imprime en orden contrario
         }
     }
 }
