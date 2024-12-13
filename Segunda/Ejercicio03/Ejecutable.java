f7adac95c3a2b5d43dfb9f7c4d733f9cfe8e8621
cris <133981266+cristinasevi@users.noreply.github.com>
2024-12-13 16:16:40 +0100

    Add files via upload

--- Segunda/Ejercicio03/Ejecutable.java
+++ Segunda/Ejercicio03/Ejecutable.java
@@ -4,11 +4,31 @@
 package Segunda.Ejercicio03;
 
 public class Ejecutable {
-    public static void main(String[] args) {
-        Nif nif1;
+    public static void main(String[] args) throws Exception{
+        Nif persona1, persona2;
         
-        nif1 = new Nif(73515833);
+        persona1 = new Nif(73515833);
+        persona2 = new Nif(73515833, 'T');
         
-        nif1.mostrar();
+        try {
+            System.out.println("Hola" + (20/0));
+        }
+        catch(ArithmeticException e) {
+            System.out.println("Se ha producido un ERROR: " + e);
+        }
+        
+        persona1.mostrar();
+        persona2.mostrar();
     }
 }
+
+/*
+ Exception -> cuando se produce un error en la ejecución del programa, se genera 
+ automáticamnte, se instancia un objeto de una de las subclases de la clase Exception.
+    
+ try -> somete a tratamiento de exceptions. Tiene que ir acompañado de un catch.
+    
+ catch -> espera recibir un parámetro de lo que tiene que hacer al recibir una exception.
+
+ throw -> lanza exception.
+*/
--- Segunda/Ejercicio03/Nif.java
+++ Segunda/Ejercicio03/Nif.java
@@ -9,8 +9,10 @@ public class Nif {
     // final -> define que es una constante
     public final char tabla[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
-    public Nif(int dni, char letra) {
+    public Nif(int dni, char letra) throws Exception { // throws Exception -> habilita el método para que lance una Exception
         this.dni = dni;
+        if(letra != tabla[dni%23])
+            throw new Exception("Has introducido mal la letra"); // mensaje que saldrá al ocurrir la Exception
         this.letra = letra;
     }
 
