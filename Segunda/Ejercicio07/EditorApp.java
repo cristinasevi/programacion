1cda49c726813ffa81f6cf4d7b17da61558e5401
cris <133981266+cristinasevi@users.noreply.github.com>
2025-01-09 09:48:48 +0100

    Add files via upload

--- Segunda/Ejercicio07/EditorApp.java
+++ Segunda/Ejercicio07/EditorApp.java
@@ -9,10 +9,19 @@ import java.awt.MenuBar;
 import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.TextArea;
+import java.awt.FileDialog;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream; 
 
 public class EditorApp extends Frame{ 
     MenuBar menubar;
     TextArea texto;
+    FileDialog abrirFichero;
+    FileDialog guardarFichero;
     
     public static void main(String[] args) {
         EditorApp app = new EditorApp();
@@ -32,6 +41,10 @@ public class EditorApp extends Frame{
         setupMenu();
         texto = new TextArea(25, 50);
         this.add("Center", texto);
+        /* indicamos donde se tiene que abrir el cuadro de diálogo, el texto que 
+           queremos poner y el tipo de plantilla que queremos para el cuadro */
+        abrirFichero = new FileDialog(this, "Abrir Fichero", FileDialog.LOAD); 
+        guardarFichero = new FileDialog(this, "Guardar Fichero", FileDialog.SAVE); 
     }
     
     private void setupMenu() {
@@ -62,8 +75,44 @@ public class EditorApp extends Frame{
                     texto.setText(" ");               
                     return true;
                 }
+                else if(ev.arg.equals("Abrir")) {
+                    abrirFichero.setVisible(true);
+                    String inFile = abrirFichero.getDirectory() + abrirFichero.getFile();
+                    leerFichero(inFile);
+                    return true;
+                }
+                else if(ev.arg.equals("Guardar")) {
+                    guardarFichero.setVisible(true);
+                    String outFile = guardarFichero.getDirectory() + guardarFichero.getFile();
+                    guardarFichero(outFile);
+                    return true;
+                }
             }
         }
         return false;
     }  
+    
+    private void leerFichero(String inFile) {
+        DataInputStream inStream;
+        try {
+            inStream = new DataInputStream(new FileInputStream(inFile));
+            String nuevo_texto = "";
+            String linea;
+            while((linea = inStream.readLine()) != null)
+                nuevo_texto += linea + "\n";
+            texto.setText(nuevo_texto);
+        }
+        catch (FileNotFoundException e) {}
+        catch(IOException e) {}
+    }
+    
+    private void guardarFichero(String outFile) {
+        DataOutputStream outStream;
+        try {
+            outStream = new DataOutputStream(new FileOutputStream(outFile));
+            outStream.writeBytes(texto.getText());
+        }
+        catch (FileNotFoundException e) {}
+        catch(IOException e) {}
+    }
 }
