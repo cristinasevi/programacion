c8d7b433f159165cb01e98bde75146f949e50590
cris <133981266+cristinasevi@users.noreply.github.com>
2025-02-11 16:34:27 +0100

    Add files via upload

--- Segunda/Ejercicio15/Columna.java
+++ Segunda/Ejercicio15/Columna.java
@@ -3,23 +3,31 @@
  */
 package Segunda.Ejercicio15;
 
+import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.Graphics;
 
 public class Columna {
+    public static final int HUECO = 80;
     public static final int ANCHURA = 30;
-    public static final int SEPARACION = 75;
-    public static final int TIEMPO = 10;
-    
+    public static final int POSINICIAL = 270;
+    public static final int VELX = -1;
     Rectangle rect1, rect2;
     
     public Columna() {
-        int altura = ((int)(Math.random()*105)+20);
-        this.rect1 = new Rectangle(270, 0, ANCHURA, altura);
-        this.rect2 = new Rectangle(270, SEPARACION, ANCHURA, altura);
+        int altura = (int)(Math.random()*130)+20; // entre 20 y 150
+        rect1 = new Rectangle(POSINICIAL, 0, ANCHURA, altura);
+        rect2 = new Rectangle(POSINICIAL, altura+HUECO, ANCHURA, 300-(altura+HUECO));
     }
     
     public void paint(Graphics g) {
-        
+        g.setColor(Color.green);
+        g.fillRect(rect1.x, rect1.y, rect1.width, rect1.height);
+        g.fillRect(rect2.x, rect2.y, rect2.width, rect2.height);
+    }
+    
+    public void update() {
+        rect1.x += VELX;
+        rect2.x += VELX;
     }
 }
--- Segunda/Ejercicio15/FlappyBird.java
+++ Segunda/Ejercicio15/FlappyBird.java
@@ -7,19 +7,24 @@ import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Event;
+import java.util.ArrayList;
 
 public class FlappyBird extends Applet implements Runnable {
+    public static final int TIEMPO = 35;
     Thread animacion;
     Image imagen;
     Graphics noseve;
     Pajaro pajaro; 
-    Columna rect1;
+    ArrayList<Columna> columnas;
+    boolean continua = true;
+    int contador = 0;
     
     public void init() {
         imagen = this.createImage(300, 300);
         noseve = imagen.getGraphics();
         pajaro = new Pajaro();
-        rect1 = new Columna();
+        columnas = new ArrayList<Columna>();
         this.setSize(300, 300);
     }
     
@@ -36,17 +41,45 @@ public class FlappyBird extends Applet implements Runnable {
         noseve.setColor(Color.BLACK);
         noseve.fillRect(0, 0, 300, 300);
         pajaro.paint(noseve);
+        for(Columna columna : columnas)
+            columna.paint(noseve);
+        
+        
+        
+        if(!continua) noseve.drawString("GAME OVER", 120, 140);
         g.drawImage(imagen, 0, 0, this);
     }
     
     public void run() {
         do {
+            contador += TIEMPO;
+            if(contador > 3000) {
+                columnas.add(new Columna());
+                contador = 0;
+            }
             pajaro.update();
+            for(Columna columna : columnas)
+                columna.update();
+            
+            if(!columnas.isEmpty()) //isEmpty
+                if(columnas.get(0).rect1.x < -30)
+                    columnas.remove(0);
+            
+            if(!continua) {
+                repaint();
+                animacion.stop();
+            }
             repaint();
             try {
-                Thread.sleep(15); 
+                Thread.sleep(TIEMPO); 
             } catch (InterruptedException e) {}
         }
         while (true);
+    }
+    
+    public boolean keyDown(Event e, int tecla) {
+        if(tecla == 32) // tecla 32 -> espacio
+            pajaro.saltar();
+        return true;
     }
 }
