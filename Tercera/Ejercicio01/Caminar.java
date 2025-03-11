26c7a80f562d8fe5dedbd73da833047daa0ebd3b
cris <133981266+cristinasevi@users.noreply.github.com>
2025-03-11 16:47:15 +0100

    Add files via upload

--- Tercera/Ejercicio01/Caminar.java
+++ Tercera/Ejercicio01/Caminar.java
@@ -5,17 +5,22 @@ package Tercera.Ejercicio01;
 
 import java.applet.Applet;
 import java.awt.Color;
+import java.awt.Event;
 import java.awt.Graphics;
 import java.awt.Image;
 
 public class Caminar extends Applet implements Runnable {
-    public static final int TIEMPO = 35;
+    public static final int TIEMPO = 100;
+    public static final int GUERRILLERO = 0;
+    public static final int HAMPON = 1;
+    public static final int VAQUERO = 2;
     Thread animacion;
     Image imagen;
     Graphics noseve;
     
     Image fotogramas[][];
     String lugares[] = {"Guerrillero/g", "Hampon/h", "Vaquero/v"};
+    DibujoAnimado dibujo;
     
     public void init() {
         imagen = this.createImage(300, 300);
@@ -26,6 +31,8 @@ public class Caminar extends Applet implements Runnable {
             for(int j=0; j<fotogramas[i].length; j++)
                 fotogramas[i][j] = getImage(getCodeBase(), "Tercera/Ejercicio01/Sprites/" + lugares[i] + (j+1) + ".gif"); // ruta y nombre con extensión del archivo
             
+        dibujo = new DibujoAnimado(fotogramas[1]);
+        
         this.setSize(300, 300);
     }
 
@@ -42,14 +49,14 @@ public class Caminar extends Applet implements Runnable {
         noseve.setColor(Color.BLACK); 
         noseve.fillRect(0, 0, 300, 300);
         
-        noseve.drawImage(fotogramas[1][2], 0, 0, 200, 250, this);
+        dibujo.paint(noseve, this);
         
         g.drawImage(imagen, 0, 0, this);
     }
     
     public void run() {
         do {
-                       
+            dibujo.update();
             
             repaint();
             try {
@@ -57,5 +64,24 @@ public class Caminar extends Applet implements Runnable {
             } catch (InterruptedException e) {}
         }
         while (true);
+    }
+    
+    public boolean keyDown(Event e, int tecla) {
+        switch (tecla) {
+        case 71: // tecla 71 o 103 -> 'g'
+        case 103:
+            dibujo = new DibujoAnimado(fotogramas[GUERRILLERO]);
+            break;
+        case 104: // tecla 72 o 104 -> 'h'
+        case 72: 
+            dibujo = new DibujoAnimado(fotogramas[HAMPON]);
+            break;
+        case 86: // tecla 86 o 118 -> 'v'
+        case 118:
+            dibujo = new DibujoAnimado(fotogramas[VAQUERO]);
+            break;
+        }
+        repaint();
+        return true;
     }
 }
--- Tercera/Ejercicio01/DibujoAnimado.java
+++ Tercera/Ejercicio01/DibujoAnimado.java
@@ -3,22 +3,31 @@
  */
 package Tercera.Ejercicio01;
 
+import java.applet.Applet;
 import java.awt.Graphics;
 import java.awt.Image;
 
 public class DibujoAnimado {
-    Image vector[];
-    int contador = 0;
+    private Image imagenes[];
+    public int indice = 0;
 
-    public DibujoAnimado(Image[] vector) {
-        this.vector = vector;
+    public DibujoAnimado(Image imagenes[]) {
+        this.imagenes = imagenes;
+    }
+
+    public Image[] getImagenes() {
+        return imagenes;
+    }
+
+    public void setImagenes(Image[] imagenes) {
+        this.imagenes = imagenes;
     }
     
-    public void paint(Graphics g) {
-        
+    public void paint(Graphics g, Applet app) {
+        g.drawImage(imagenes[indice], 0, 0, 200, 250, app);
     }
     
     public void update() {
-        
+        indice = (indice + 1) % imagenes.length;
     }
 }
