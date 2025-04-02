019992a173e917dfae0b1a5d4f2aa1444176c784
cris <133981266+cristinasevi@users.noreply.github.com>
2025-04-02 10:12:16 +0200

    Add files via upload

--- Tercera/Ejercicio05/BuscaMina.java
+++ Tercera/Ejercicio05/BuscaMina.java
@@ -31,7 +31,42 @@ public class BuscaMina extends Applet {
             for(int j=0; j<DIM; j++)
                 casillas[i][j] = new Casilla(100+(j*Casilla.TAM), 100+(i*Casilla.TAM), reverso);
         
+        int aleatorios[] = obtenerAleatorio();
+        
+        for (int i=0; i<DIM; i++)
+            casillas[aleatorios[i]/DIM][aleatorios[i]%DIM].setMina(mina);
+        
+        for (int fila=0; fila<DIM; fila++) {
+            for(int columna=0; columna<DIM; columna++) {
+                int inicioX = (fila == 0) ? 0 : fila-1;
+                int finX = (fila == DIM-1) ? DIM-1 : fila+1;
+                int inicioY = (columna == 0) ? 0 : columna-1;
+                int finY = (columna == DIM-1) ? DIM-1 : columna+1;
+                int contador = 0;
+                for(int i=inicioX; i<=finX; i++) {
+                    for(int j=inicioY; j<=finY; j++) {
+                        if(casillas[i][j].getMina() != null)
+                            contador++;
+                    }
+                casillas[fila][columna].setAlrededor(contador);
+                }
+            }
+        }
+        
         this.setSize(400, 500);
+    }
+
+    public int[] obtenerAleatorio() {
+        int resultados[] = new int[DIM];
+        int vector[] = new int[DIM*DIM];
+        for(int i=0; i<(DIM*DIM); i++)
+            vector[i] = i;
+        for(int i=0; i<DIM; i++) {
+            int aleatorio = (int)(Math.random() * ((DIM*DIM)-i));
+            resultados[i] = vector[aleatorio];
+            vector[aleatorio] = vector[(DIM*DIM)-i-1];
+        }
+        return resultados;
     }
 
     public void update(Graphics g) {
